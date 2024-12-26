4ed4bd64bcbdddc0f7bed15f41beb2a23d74dd8a
TaKieuLong <takieulong@gmail.com>
2024-12-26 22:50:23 +0700

    test circle ci

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./src/app.js");
 require("dotenv").config();
 const router = require("./src/routers/router");
-const PORT = process.env.PORT || 8001;
+const PORT = 8002;
 
 app.use("/", router);
 
